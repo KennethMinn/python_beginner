0ee2532a33622757fe09c3d2850de71630f583cc
KennethMinn <minthukha2472005@gmail.com>
2024-05-14 14:25:45 +0630

    Type Conversion

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('hello world')
+# birth_year = input("Enter your birth year : ") #will always return string
+# age = 2024 - int(birth_year)
+# print(age)
+
+first = input('First : ')
+second = input('Second : ')
+sum = float(first) + float(second)
+print(sum)
